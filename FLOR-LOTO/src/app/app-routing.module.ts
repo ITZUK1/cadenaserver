e1256e57572baf4648ca511ff66355c785040993
Miguel <mixato86@gmail.com>
2024-12-12 12:49:02 -0500

    se cambio el ruteo y se elimino el spec.ts

--- FLOR-LOTO/src/app/app-routing.module.ts
+++ FLOR-LOTO/src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { PantallaFlorComponent } from './components/pantalla-flor/pantalla-flor.
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
   { path: 'home', component: PantallaInicioComponent },
-  { path: 'about', component: PantallaVariedadComponent },
-  { path: 'contact', component: PantallaFlorComponent },
+  { path: 'variety', component: PantallaVariedadComponent },
+  { path: 'flower', component: PantallaFlorComponent },
   { path: '**', redirectTo: '/home' } 
 ];
 
