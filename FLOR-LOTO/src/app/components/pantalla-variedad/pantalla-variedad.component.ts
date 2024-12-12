87a7b43b6d39f4fc605ebabc5dcbcb658958806c
Miguel <mixato86@gmail.com>
2024-12-12 14:03:32 -0500

    se agrego variedad

--- FLOR-LOTO/src/app/components/pantalla-variedad/pantalla-variedad.component.ts
+++ FLOR-LOTO/src/app/components/pantalla-variedad/pantalla-variedad.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./pantalla-variedad.component.css']
 })
 export class PantallaVariedadComponent {
+  flores = [
+    {
+      nombre: 'Rosa',
+      descripcion: 'Una flor clásica con colores vibrantes.',
+      imagen: 'assets/img/flor.png'
+    },
+    {
+      nombre: 'Tulipán',
+      descripcion: 'Elegante y perfecta para la primavera.',
+      imagen: 'assets/tulipan.jpg'
+    },
+    // Agrega más flores aquí
+  ];
 
+  verFlor(flor: any): void {
+    console.log('Información de la flor:', flor);
+    alert(`Has seleccionado la flor: ${flor.nombre}`);
+  }
 }
